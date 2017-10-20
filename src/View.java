8204a2cd08480249707fcd48590a9cf5e50b7a90
MarkWang <nark29880420@gmail.com>
2017-10-20 14:28:17 +0800

    group layout is finish!!!!

--- src/View.java
+++ src/View.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.LayoutStyle;
 
 import oracle.jrockit.jfr.JFR;
 
@@ -23,7 +24,7 @@ public class View {
 	}
 	
 	private void initView() {
-		jFrame=new JFrame();
+		jFrame=new JFrame("ReentrantReakWriteLock");
 		jPanel=new JPanel();
 		jTextArea=new JTextArea();
 		readButton=new JButton("Read");
@@ -31,75 +32,60 @@ public class View {
 		writeButton=new JButton("Write");
 		cancelWriteButton=new JButton("CancelWrite");
 		
+		jFrame.setLocation(300, 300);
+		
+		jPanel.setPreferredSize(new Dimension(350, 300));
+
 		GroupLayout groupLayout=new GroupLayout(jPanel);
 		jPanel.setLayout(groupLayout);
-		
-		groupLayout.setHorizontalGroup(
-				groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup()
-							.addComponent(jTextArea)
-							.addGroup(groupLayout.createSequentialGroup()
-									.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-											.addComponent(readButton,GroupLayout.PREFERRED_SIZE,GroupLayout.PREFERRED_SIZE,GroupLayout.PREFERRED_SIZE)
-											.addComponent(cancelReadButton)
-											)
-									.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-											.addComponent(writeButton)
-											.addComponent(cancelWriteButton)
-											)
-									)
-							)	
-				);
-		
-		groupLayout.setVerticalGroup(
-				groupLayout.createSequentialGroup()
-					.addComponent(jTextArea)
-					.addGroup(groupLayout.createParallelGroup()
-							.addComponent(readButton)
-							.addComponent(writeButton)
-							)
-					.addGroup(groupLayout.createParallelGroup()
-							.addComponent(cancelReadButton)
-							.addComponent(cancelWriteButton)
-							)
-				);
-		jPanel.setSize(new Dimension(300, 400));
-		jFrame.add(jPanel);
-		jFrame.pack();
-		jFrame.setVisible(true);
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
 	
-	private void initView2() {
-		jFrame=new JFrame();
-		jPanel=new JPanel();
-		jTextArea=new JTextArea();
-		readButton=new JButton("Read");
-		cancelReadButton=new JButton("CancelRead");
-		writeButton=new JButton("Write");
-		cancelWriteButton=new JButton("CancelWrite");
-		
-		GroupLayout groupLayout=new GroupLayout(jPanel);
-		jPanel.setLayout(groupLayout);
-		
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup()
-				.addComponent(jTextArea)
-				.addGroup(groupLayout.createSequentialGroup()
-						.addComponent(readButton)
-						.addComponent(writeButton)
-				)	
-		);
+				groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup()
+							.addGroup(groupLayout.createSequentialGroup()
+									.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+						                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(jTextArea,0,300,GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+						                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									)
+							.addGroup(groupLayout.createSequentialGroup()
+									 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+						                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+											.addComponent(readButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+											.addComponent(cancelReadButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+											)
+									 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+						                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+											.addComponent(writeButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+											.addComponent(cancelWriteButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+											)
+									 .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+						                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									)
+							)
+				);
 		
 		groupLayout.setVerticalGroup(
-			groupLayout.createSequentialGroup()
-				.addComponent(jTextArea)
-				.addGroup(groupLayout.createSequentialGroup()
-						.addComponent(readButton)
-						.addComponent(writeButton)
-				)
-		);
-//		
+				groupLayout.createSequentialGroup()
+					.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+		                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(jTextArea,0,200,GroupLayout.PREFERRED_SIZE)
+					.addGap(15)
+					.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+							.addComponent(readButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+							.addComponent(writeButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+							)
+					.addGap(15)
+					.addGroup(groupLayout.createParallelGroup()
+							.addComponent(cancelReadButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+							.addComponent(cancelWriteButton,0,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE)
+							)
+					.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+		                     GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+				);
 		jFrame.add(jPanel);
 		jFrame.pack();
 		jFrame.setVisible(true);
